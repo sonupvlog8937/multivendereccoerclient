e224b38a5103b2952b10fd5c77eec2dd99af7c78
sonupvlog8937 <sonupvlog8937@gmail.com>
2025-05-24 21:17:57 +0000

    xx

--- src/components/Headers.jsx
+++ src/components/Headers.jsx
@@ -49,7 +49,7 @@ const Headers = () => {
                                 <span><GrMail /></span>
                                 <span>sk4562944@gmail.com</span>
                             </li>
-                            <span>Online Shopping App ggrre</span>
+                            <span>Online Shopping App</span>
                         </ul>
                         <div>
                             <div className='flex justify-center items-center gap-10'>
@@ -86,7 +86,7 @@ const Headers = () => {
                     <div className='h-[80px] md-lg:h-[100px] flex justify-between items-center flex-wrap'>
                         <div className='md-lg:w-full w-3/12 md-lg:pt-4'>
                             <div className='flex justify-between items-center'>
-                                <Link to='/' className="-h[50px] w-[75px]">
+                                <Link to='/' className="h-[50px] w-[75px]">
                                     <img className='h-25% w-50%' src="/images/logo.png" alt="logo" />
                                 </Link>
                                 <div className='justify-center items-center w-[30px] h-[30px] bg-white text-slate-600 border border-slate-600 rounded-sm cursor-pointer lg:hidden md-lg:flex xl:hidden hidden' onClick={() => setShowShidebar(false)}>
