dd2887e2ec20bc42bf69b65a139db36fcb9228f3
Sonu Kumar <sonupvlog8937@gmail.com>
2025-05-25 15:16:59 +0530

    ytu

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import Heders from '../components/Headers'
 import Banner from '../components/Banner'
@@ -7,31 +7,19 @@ import FeatureProducts from '../components/products/FeatureProducts'
 import Products from '../components/products/Products'
 import { Link } from 'react-router-dom'
 import Footer from '../components/Footer'
-import Pagination from '../components/Pagination'
-import { get_category, get_products, query_products } from '../store/reducers/homeReducer'
+import { get_category, get_products } from '../store/reducers/homeReducer'
 const Home = () => {
     const dispatch = useDispatch()
-    const [pageNumber, setPageNumber] = useState(1)
-    const { products, totalProduct, parPage, latest_product, topRated_product, discount_product } = useSelector(state => state.home)
+    const {products, latest_product, topRated_product, discount_product } = useSelector(state => state.home)
     useEffect(() => {
         dispatch(get_products())
     }, [])
-
-    useEffect(() => {
-        dispatch(
-            query_products({
-                pageNumber
-            })
-        )
-    }, [pageNumber])
-
-    
     return (
         <div className='w-full'>
             <Heders />
             <Banner />
             <div className='flex item-center justify-center'>
-                <Link className='px-5 w-[146px] py-2 bg-blue-500 rounded-sm text-white' to='/shops'>Shop Now</Link>
+                <Link className='px-5 w-[145px] py-2 bg-blue-500 rounded-sm text-white' to='/shops'> Shop Now</Link>
             </div>
             <div className='my-4 max-w-[1440px] mx-auto px-16 sm:px-5 md-lg:px-12 md:px-10'>
                 <Categorys />
@@ -39,13 +27,6 @@ const Home = () => {
             <div className='py-[45px] max-w-[1440px] mx-auto px-16 sm:px-5 md-lg:px-12 md:px-10'>
                 <FeatureProducts products={products} />
             </div>
-            <div>sonu kumar raj </div>
-            <div>
-                
-            <Pagination pageNumber={pageNumber} setPageNumber={setPageNumber} totalItem={totalProduct} parPage={parPage} showItem={Math.floor(totalProduct / parPage)} />
-                
-            </div>
-            sonu kumar
             <div className='py-10'>
                 <div className='max-w-[1440px] mx-auto px-16 sm:px-5 md-lg:px-12 md:px-10 flex flex-wrap'>
                     <div className="grid w-full grid-cols-3 md-lg:grid-cols-2 md:grid-cols-1 gap-7">
